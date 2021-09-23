309a29ff3ea7ba1b6a5a5bfbcd159c1f5c1c9bb7
QuinnBernardi <90325664+Qbernardi@users.noreply.github.com>
2021-09-23 12:56:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,241 @@
+# Programmer: Mr. Devet
+# Date: September 9, 2021
+# Description: Draws a scene with a boat and a sun
+
+# Load the contents of the turtle module into the python
+# environment
+from pygameplus import *
+
+# Create the graphics window and open it
+screen = Screen(725, 310, "My Boat")
+screen.background_color = "sky blue"
+screen.open()
+
+# Change the turtle's shape to be a turtle, rather than
+# an arrow
+mikey = Turtle()
+screen.add(mikey)
+mikey.line_width = 30
+
+# Uncomment to change the speed of the turtle
+mikey.speed = 700
+mikey.turn_right(90)
+mikey.move_forward(190)
+mikey.begin_line()
+mikey.turn_left(90)
+mikey.move_forward(500)
+mikey.turn_left(90)
+mikey.move_forward(30)
+mikey.turn_left(90)
+mikey.line_color = "red"
+mikey.move_forward(1000)
+mikey.turn_right(90)
+mikey.move_forward(30)
+mikey.turn_right(90)
+mikey.line_color = "white"
+mikey.move_forward(1000)
+mikey.turn_left(90)
+mikey.move_forward(30)
+mikey.turn_left(90)
+mikey.line_color = "red"
+mikey.move_forward(1000)
+mikey.turn_right(90)
+mikey.move_forward(30)
+mikey.turn_right(90)
+mikey.line_color = "white"
+mikey.move_forward(1000)
+mikey.turn_left(90)
+mikey.move_forward(30)
+mikey.turn_left(90)
+mikey.line_color = "red"
+mikey.move_forward(1000)
+mikey.turn_right(90)
+mikey.move_forward(30)
+mikey.turn_right(90)
+mikey.line_color = "white"
+mikey.move_forward(1000)
+mikey.turn_left(90)
+mikey.move_forward(30)
+mikey.turn_left(90)
+mikey.line_color = "red"
+mikey.move_forward(590)
+mikey.line_color = "blue"
+mikey.move_forward(51.25)
+mikey.line_color = "white"
+mikey.circle(5,360)
+mikey.line_color = "blue"
+mikey.move_forward(51.25)
+mikey.line_color = "white"
+mikey.circle(5,360)
+mikey.line_color = "blue"
+mikey.move_forward(51.25)
+mikey.line_color = "white"
+mikey.circle(5,360)
+mikey.line_color = "blue"
+mikey.move_forward(51.25)
+mikey.line_color = "white"
+mikey.circle(5,360)
+mikey.line_color = "blue"
+mikey.move_forward(51.25)
+mikey.line_color = "white"
+mikey.circle(5,360)
+mikey.line_color = "blue"
+mikey.move_forward(51.25)
+mikey.line_color = "white"
+mikey.circle(5,360)
+mikey.line_color = "blue"
+mikey.move_forward(51.25)
+mikey.line_color = "white"
+mikey.circle(5,360)
+mikey.line_color = "blue"
+mikey.move_forward(51.25)
+mikey.line_color = "white"
+mikey.circle(5,360)
+mikey.turn_right(90)
+mikey.move_forward(30)
+mikey.turn_right(90)
+mikey.line_color = "blue"
+mikey.move_forward(51.25)
+mikey.line_color = "white"
+mikey.circle(5,360)
+mikey.line_color = "blue"
+mikey.move_forward(51.25)
+mikey.line_color = "white"
+mikey.circle(5,360)
+mikey.line_color = "blue"
+mikey.move_forward(51.25)
+mikey.line_color = "white"
+mikey.circle(5,360)
+mikey.line_color = "blue"
+mikey.move_forward(51.25)
+mikey.line_color = "white"
+mikey.circle(5,360)
+mikey.line_color = "blue"
+mikey.move_forward(51.25)
+mikey.line_color = "white"
+mikey.circle(5,360)
+mikey.line_color = "blue"
+mikey.move_forward(51.25)
+mikey.line_color = "white"
+mikey.circle(5,360)
+mikey.line_color = "blue"
+mikey.move_forward(51.25)
+mikey.line_color = "white"
+mikey.circle(5,360)
+mikey.line_color = "blue"
+mikey.move_forward(51.25)
+mikey.line_color = "white"
+mikey.circle(5,360)
+mikey.line_color = "white"
+mikey.move_forward(590)
+mikey.turn_left(90)
+mikey.move_forward(30)
+mikey.turn_left(90)
+mikey.line_color = "red"
+mikey.move_forward(590)
+mikey.line_color = "blue"
+mikey.move_forward(51.25)
+mikey.line_color = "white"
+mikey.circle(5,360)
+mikey.line_color = "blue"
+mikey.move_forward(51.25)
+mikey.line_color = "white"
+mikey.circle(5,360)
+mikey.line_color = "blue"
+mikey.move_forward(51.25)
+mikey.line_color = "white"
+mikey.circle(5,360)
+mikey.line_color = "blue"
+mikey.move_forward(51.25)
+mikey.line_color = "white"
+mikey.circle(5,360)
+mikey.line_color = "blue"
+mikey.move_forward(51.25)
+mikey.line_color = "white"
+mikey.circle(5,360)
+mikey.line_color = "blue"
+mikey.move_forward(51.25)
+mikey.line_color = "white"
+mikey.circle(5,360)
+mikey.line_color = "blue"
+mikey.move_forward(51.25)
+mikey.line_color = "white"
+mikey.circle(5,360)
+mikey.line_color = "blue"
+mikey.move_forward(51.25)
+mikey.line_color = "white"
+mikey.circle(5,360)
+mikey.turn_right(90)
+mikey.move_forward(30)
+mikey.turn_right(90)
+mikey.line_color = "blue"
+mikey.move_forward(51.25)
+mikey.line_color = "white"
+mikey.circle(5,360)
+mikey.line_color = "blue"
+mikey.move_forward(51.25)
+mikey.line_color = "white"
+mikey.circle(5,360)
+mikey.line_color = "blue"
+mikey.move_forward(51.25)
+mikey.line_color = "white"
+mikey.circle(5,360)
+mikey.line_color = "blue"
+mikey.move_forward(51.25)
+mikey.line_color = "white"
+mikey.circle(5,360)
+mikey.line_color = "blue"
+mikey.move_forward(51.25)
+mikey.line_color = "white"
+mikey.circle(5,360)
+mikey.line_color = "blue"
+mikey.move_forward(51.25)
+mikey.line_color = "white"
+mikey.circle(5,360)
+mikey.line_color = "blue"
+mikey.move_forward(51.25)
+mikey.line_color = "white"
+mikey.circle(5,360)
+mikey.line_color = "blue"
+mikey.move_forward(51.25)
+mikey.line_color = "white"
+mikey.circle(5,360)
+mikey.line_color = "white"
+mikey.move_forward(590)
+mikey.turn_left(90)
+mikey.move_forward(30)
+mikey.turn_left(90)
+mikey.line_color = "red"
+mikey.move_forward(590)
+mikey.line_color = "blue"
+mikey.move_forward(51.25)
+mikey.line_color = "white"
+mikey.circle(5,360)
+mikey.line_color = "blue"
+mikey.move_forward(51.25)
+mikey.line_color = "white"
+mikey.circle(5,360)
+mikey.line_color = "blue"
+mikey.move_forward(51.25)
+mikey.line_color = "white"
+mikey.circle(5,360)
+mikey.line_color = "blue"
+mikey.move_forward(51.25)
+mikey.line_color = "white"
+mikey.circle(5,360)
+mikey.line_color = "blue"
+mikey.move_forward(51.25)
+mikey.line_color = "white"
+mikey.circle(5,360)
+mikey.line_color = "blue"
+mikey.move_forward(51.25)
+mikey.line_color = "white"
+mikey.circle(5,360)
+mikey.line_color = "blue"
+mikey.move_forward(51.25)
+mikey.line_color = "white"
+mikey.circle(5,360)
+mikey.line_color = "blue"
+mikey.move_forward(51.25)
+mikey.line_color = "white"
+mikey.circle(5,360)
